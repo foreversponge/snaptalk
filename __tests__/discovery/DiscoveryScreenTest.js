840575aea9bb57af722b471adbadcb22382bfc34
Sam <huangs.wk.0508@gmail.com>
2020-04-06 11:24:10 -0400

    Created unit tests for Discovery/Home screens #54
    Created render tests for these screens.

---
+++ __tests__/discovery/DiscoveryScreenTest.js
@@ -0,0 +1,12 @@
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import DiscoveryScreen from '../components/discovery/DiscoveryScreen'
+
+describe('DiscoveryScreen Test', () => {
+    let discoveryScreenSnapshot = renderer.create(<DiscoveryScreen />).toJSON();
+
+    test('DiscoveryTest Render Test', () => {
+        expect(discoveryScreenSnapshot).toMatchSnapshot();
+    })
+})
---
+++ __tests__/personal/HomeScreenTest.js
@@ -0,0 +1,13 @@
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import HomeScreen from '../components/home/HomeScreen'
+
+describe('HomeScreen Test', () => {
+    let personalScreenSnapshot = renderer.create(<HomeScreen />).toJSON();
+
+    test('HomeTest Render Test', () => {
+        expect(homeScreenSnapshot).toMatchSnapshot();
+    })
+})
+
