b2f4570aadd6b1f78ac36fcd8264f662ffd2d435
Razvan Ivan <razvan.ivan10@yahoo.com>
2020-04-09 17:15:20 -0400

    Added fix for the profile pic not showing up. Finished refactoring #53 .

--- components/firebase/AuthenticationController.js
+++ components/firebase/AuthenticationController.js
@@ -74,6 +74,29 @@ class AuthenticationController {
     }
   };
 
+  uploadPhotoAsync = async (uri, filename) => {
+    return new Promise(async (res, rej) => {
+      const response = await fetch(uri);
+      const file = await response.blob();
+
+      let upload = Firebase.storage()
+        .ref(filename)
+        .put(file);
+
+      upload.on(
+        'state_changed',
+        snapshot => {},
+        err => {
+          rej(err);
+        },
+        async () => {
+          const url = await upload.snapshot.ref.getDownloadURL();
+          res(url);
+        },
+      );
+    });
+  };
+
   get firestore() {
     return Firebase.firestore();
   }
